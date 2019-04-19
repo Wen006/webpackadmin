84011f271967475af03ba9f06560bbc431527332
Wen006 <1432316105@qq.com>
2019-04-19 14:22:16 +0800

    打包多个文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+//   entry: './src/index.js',
+  entry:{
+      app:'./src/index.js',
+      print:'./src/print.js'
+  },
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    filename:'[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader'
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+            'file-loader'
+            ]
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+            'file-loader'
+            ]
+        }
+    ]
   }
 };
